a211f02a7742e99362921465b3518ce32b7b0670
raghu0891 <raghu0891@gmail.com>
2023-06-01 16:25:30 +0530

    POR mainnet codebase

---
+++ README.md
@@ -0,0 +1,6 @@
+# deposit-widget
+
+pre-requisites  
+do setup XDCPay Chrome Extension in your chrome  
+
+# Check Instructions.pdf provided in this directory for detailed steps to follow
---
+++ consumer-contract/ConsumerContract.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+
+
+interface IInvokeOracle {
+   function requestData(address _authorizedWalletAddress) external returns (uint256 requestId);
+   function showPrice(uint256 _reqid) external view returns (uint256 answer, uint256 updatedOn);
+}
+contract ConsumerContract {
+   address CONTRACTADDR = 0xA6c0B2442d3829eeeaa69F7351bAd4D68C4A5F09;
+   uint256 public requestId;
+   address private owner;
+   constructor() public{
+       owner = msg.sender;
+   }
+   //Fund this contract with sufficient PLI, before you trigger below function.
+   //Note, below function will not trigger if you do not put PLI in above contract address
+   function getPriceInfo() external returns (uint256) {
+       require(msg.sender==owner,"Only owner can trigger this");
+       (requestId) = IInvokeOracle(CONTRACTADDR).requestData({_authorizedWalletAddress:owner});
+       return requestId;
+   }
+   //TODO - you can customize below function as you want, but below function will give you the pricing value
+   //This function will give you last stored value in the contract
+   function show(uint256 _id) external view returns (uint256, uint256) {
+       (uint256 answer, uint256 updatedOn) = IInvokeOracle(CONTRACTADDR).showPrice({_reqid: _id});
+       return (answer,updatedOn);
+   }
+}
+
